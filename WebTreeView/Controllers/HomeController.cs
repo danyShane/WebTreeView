2f9a0586056fa7631cf0aa2c8669979c99a8afd7
danyShane <peruchan_clan@hotmail.com>
2019-07-03 10:15:29 +0200

    first commit upload all files

---
+++ WebTreeView/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using WebTreeView.Models;
+
+namespace WebTreeView.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<string> lista = new List<string>();
+        private string cadena = "";
+
+        public IActionResult Index()
+        {
+            //obtenemos los datos del fichero de configuracion
+            //var items = JArray.Parse(System.IO.File.ReadAllText(Directory.GetCurrentDirectory() + "/Items.json"));
+            //var builder = new ConfigurationBuilder()
+            //      .SetBasePath(Directory.GetCurrentDirectory())
+            //      .AddJsonFile("Items.json", optional: true);
+
+            //var items = builder.Build();
+            string lines = "";
+            object lockToread = new object();
+
+            lock (lockToread)
+            {
+                FileStream fileStream = new FileStream("Items.json", FileMode.Open);
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    lines = reader.ReadToEnd();
+                }                               
+            }
+
+            JToken jToken = JToken.Parse(lines);
+
+            JToken[] padre = jToken.Children().ToArray();            
+
+            load(padre);
+
+            var lista2 = lista;
+
+            int cont = jToken.Children().Count();                                        
+
+            ViewData["items"] = cadena;
+            return View();
+            
+        }
+
+        private void load(JToken[] padre)
+        {
+            bool ul = false;
+
+            if (padre.Count() > 0)
+            {
+                cadena = cadena + "<ul>";
+                ul = true;
+            }           
+            
+            foreach (var hijo in padre)
+            {                
+                if (null != hijo.SelectToken("Name") )
+                {
+                    cadena = cadena + "<li>" + hijo.SelectToken("Name").ToString() + "</li>";
+                    lista.Add(hijo.SelectToken("Name").ToString());
+                    load(hijo.SelectToken("Children").ToArray());
+                }
+                else
+                {
+                    load(hijo.Children().ToArray());
+                }
+            }
+            if (ul) cadena = cadena + "</ul>";
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+} 
---
+++ WebTreeView/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebTreeView.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
